b65b3be903c0952b0c1572e26dae07242b98ab1c
Mateo Garcia <mxg1122@gmail.com>
2026-02-18 12:20:49 -0600

    adjustment of file name

--- Java/HelloWorld_Krish.java
+++ Java/HelloWorld_Krish.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class HelloWorld_Krish {
     public static void main(String[] args) {
         System.out.println("Hello, World");
     }
--- Java/ID24_Mateo.java
+++ Java/ID24_Mateo.java
@@ -1,4 +1,4 @@
-public class ID24Mateo { 
+public class ID24_Mateo { 
         public static void main(String[] args) 
         {
         System.out.println("Hello, World");
